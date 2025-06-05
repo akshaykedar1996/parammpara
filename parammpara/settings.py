44ceae293e7dca19768d3140ae24d40187e197d1
Akshay kedar <akshaykedar1996@gmail.com>
2025-06-05 10:09:16 +0530

    commit19

--- parammpara/settings.py
+++ parammpara/settings.py
@@ -125,7 +125,8 @@ STATIC_URL = '/static/'
 if DEBUG:
   #  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     STATIC_URL = 'static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
     STATICFILES_DIRS = [
         BASE_DIR / "static",
